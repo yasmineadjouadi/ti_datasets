d7250102a2b944a7b08c84f96b820834c8afbf0b
yvsmiinnee <147881214+yasmineadjouadi@users.noreply.github.com>
2026-03-15 20:32:45 +0100

    Optimize hash processing and output file management
    Refactor to check for existing hashes before processing and update output file handling.

--- dt.py
+++ dt.py
@@ -3,31 +3,37 @@ import json
 import random
 import time
 from concurrent.futures import ThreadPoolExecutor
+import os
 
 URL = "https://raw.githubusercontent.com/romainmarcoux/malicious-hash/refs/heads/main/full-hash-sha256-aa.txt"
 
 LIMITS = 2000
 
 API_KEYS = [
-"afaa2f23aff1727b8625ab49c95d6c603f1a4d6f33040308e74711c0099b1b0d",
-"042268ef6f012e4b8edb42f681e047bf1d67372aab5f12c8833d1a9496cfe2f0"
+"af8aeba02657ebe85cac81c8f3e1aa64a9a97e2c40db940e35d8675f8028f610",
+"5ffb7905f7b959e5b0964c8bcb5f0d72277e7b449ee445b7fa126228858add61"
 ]
 
 OUTPUT_FILE = "hash_dataset.json"
 
-SLEEP_API = 15
-
 results = []
 
+existing_data = []
+existing_hashes = set()
+
+if os.path.exists(OUTPUT_FILE):
+    with open(OUTPUT_FILE, "r", encoding="utf-8") as f:
+        try:
+            existing_data = json.load(f)
+            existing_hashes = {item["value"] for item in existing_data}
+        except:
+            existing_data = []
 
 def function_virustotal(hash_value, api_key):
-
     url = f"https://www.virustotal.com/api/v3/files/{hash_value}"
-
     headers = {"x-apikey": api_key}
 
     try:
-
         response = requests.get(url, headers=headers, timeout=10)
 
         if response.status_code != 200:
@@ -60,13 +66,15 @@ def function_virustotal(hash_value, api_key):
 
 
 def worker(hash_value):
+    if hash_value in existing_hashes:
+        return
 
     for key in API_KEYS:
-
         result = function_virustotal(hash_value, key)
 
         if result:
             results.append(result)
+            existing_hashes.add(hash_value)
             return
 
         time.sleep(1)
@@ -82,13 +90,13 @@ all_hashes = response.text.splitlines()
 
 hashes = random.sample(all_hashes, LIMITS)
 
-
 with ThreadPoolExecutor(max_workers=2) as executor:
     executor.map(worker, hashes)
 
+existing_data.extend(results)
 
-with open(OUTPUT_FILE, "w") as f:
-    json.dump(results, f, indent=4)
+with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
+    json.dump(existing_data, f, indent=4)
 
-
-print(len(results), "malicious hashes saved to", OUTPUT_FILE)
+print(len(results), "new hashes added")
+print(len(existing_data), "total hashes in dataset")
